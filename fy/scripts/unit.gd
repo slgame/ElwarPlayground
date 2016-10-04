1a124ede8902e7cc7ca8733db06252145c42b742
fy <fy0@qq.com>
2016-10-05 02:04:04 +0800

    添加了游戏中<单位>的类

---
+++ fy/scripts/unit.gd
@@ -0,0 +1,18 @@
+
+extends Node
+
+# 单位，建筑或兵种
+
+var speed = 100 # 移动速度
+var name = "DOGE" # 单位名称
+var atk_type = 0 # 无/近战/远程
+var atk_interval = 1 # 攻击间隔(1s)
+var damage = 1 # 伤害
+var hp = 5 # 生命值
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
